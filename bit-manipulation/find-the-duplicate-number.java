a6c5766aeb0280ed995c31872841f328f51016cf
Omkar Shinde <70501103+omkarshinde1711@users.noreply.github.com>
2025-07-16 01:08:59 +0530

    LC-find-the-duplicate-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/find-the-duplicate-number.java
+++ bit-manipulation/find-the-duplicate-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         int n = nums.length;
-        for ( int i = 0 ; i < n ; i++ ){
+        for ( int i = 0 ; i < n-1 ; i++ ){
             int count = 0;
             for ( int j = i + 1 ; j < n ; j++){
                 if (nums[i] == nums[j]){
