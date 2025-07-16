3405f650ef597bec2fa67eef186fda0a311bb281
Omkar Shinde <70501103+omkarshinde1711@users.noreply.github.com>
2025-07-17 01:32:00 +0530

    LC-sort-colors [Runtime: 3 ms; Memory: 42.1 MB]

--- two-pointers/sort-colors.java
+++ two-pointers/sort-colors.java
@@ -1,6 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         // Insertion Sort Approch 
+
         // int i,j,key;
         // int n = nums.length;
         // for ( i = 1 ; i < n ; i++){
@@ -14,24 +15,48 @@ class Solution {
         //     }
         // }
 
+        //---
+
         // Selection Sort
+        // int n = nums.length;
+        //     for ( int i = 0 ; i < n ; i++){
+
+        //         int minIndex = i;   //current index ko minimum consider karo
+
+        //         for ( int j = i+1 ; j < n ; j++){
+        //             if(nums[j] < nums[minIndex])  //check if koi aur minimum hai kya
+        //             minIndex = j;                 //usko minIndex bana do
+        //         }
+
+        //         //only swap to right place means at index j 
+        //         //if minIndex i nahi ho toh.... 
+        //         if (minIndex != i){
+        //                 int temp = nums[i];
+        //                 nums[i] = nums[minIndex];
+        //                 nums[minIndex] = temp;
+        //             }
+        //     }
+        //---
+
+        // Bubble Sort
         int n = nums.length;
-            for ( int i = 0 ; i < n ; i++){
+        for (int i = 0; i < n-1 ; i++){
 
-                int minIndex = i;   //current index ko minimum consider karo
+            //optamization
+            boolean swapped = false;
 
-                for ( int j = i+1 ; j < n ; j++){
-                    if(nums[j] < nums[minIndex])  //check if koi aur minimum hai kya
-                    minIndex = j;                 //usko minIndex bana do
+
+            for(int j = 0; j < n-i-1 ; j++){
+                if (nums[j] > nums [j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp; 
+                    swapped = true;
                 }
-
-                //only swap to write place means at index j 
-                //if minIndex i nahi ho toh.... 
-                if (minIndex != i){
-                        int temp = nums[i];
-                        nums[i] = nums[minIndex];
-                        nums[minIndex] = temp;
-                    }
             }
+
+            if(!swapped) break;  //nothing swapped , hence arr is already sprted
+        }
+
     }
 }
