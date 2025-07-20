2f3d59d317e1b522179be1e24dfed6f9853f6406
Omkar Shinde <70501103+omkarshinde1711@users.noreply.github.com>
2025-07-20 16:10:15 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock.java
+++ array/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,15 @@
 class Solution {
     
-    static int num , maxNum;
-    static int splitindex;
-    
     public int maxProfit(int[] prices) {
+        int num = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
         if(prices.length == 1){
             return 0;
         }
 
         //find first minium number 
         num = prices[0]; 
+        int splitindex = 0;
         for(int i = 0; i < prices.length; i++){
             if ( num > prices[i]){
                 num = prices[i];
@@ -25,6 +25,7 @@ class Solution {
             return 0;
         }
         else{
+        System.out.println("max " + maxNum);            
         for (int j = splitindex; j < prices.length; j++){
             if ( maxNum < prices[j]){
                 maxNum = prices[j];
@@ -32,7 +33,7 @@ class Solution {
         }
         return(maxNum - num);
         }
-        //System.out.println("max " + maxNum);            
+        
     //return -1;
     }
 }
