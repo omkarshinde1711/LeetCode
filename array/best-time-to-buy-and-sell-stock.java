a2a93f5686da18afacd6b04d3169c6d63eb98c50
Omkar Shinde <70501103+omkarshinde1711@users.noreply.github.com>
2025-07-20 16:21:51 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: 2 ms; Memory: 62.2 MB]

--- array/best-time-to-buy-and-sell-stock.java
+++ array/best-time-to-buy-and-sell-stock.java
@@ -1,39 +1,25 @@
 class Solution {
     
     public int maxProfit(int[] prices) {
-        int num = Integer.MAX_VALUE;
-        int maxNum = Integer.MIN_VALUE;
-        if(prices.length == 1){
+        //edge case
+        if ( prices == null || prices.length <= 1){
             return 0;
         }
+        int minPrice = prices[0]; // initializing to the first day
+        int maxProfit = 0; // no profit so far
 
-        //find first minium number 
-        num = prices[0]; 
-        int splitindex = 0;
-        for(int i = 0; i < prices.length; i++){
-            if ( num > prices[i]){
-                num = prices[i];
-                splitindex = i;
+        for ( int i = 1; i < prices.length; i++){
+            //whats current profit 
+            int currentProfit = prices[i] - minPrice;
+
+            if( currentProfit > maxProfit){
+                maxProfit = currentProfit;
+            }
+
+            if ( prices[i] < minPrice){
+                minPrice = prices[i];
             }
         }
-        //debug
-        System.out.println("num " + num);
-        System.out.println("split " + splitindex); 
-        //System.out.println(prices.length);
-        if (splitindex == prices.length-1)
-        {
-            return 0;
-        }
-        else{
-        System.out.println("max " + maxNum);            
-        for (int j = splitindex; j < prices.length; j++){
-            if ( maxNum < prices[j]){
-                maxNum = prices[j];
-            }            
-        }
-        return(maxNum - num);
-        }
-        
-    //return -1;
+    return maxProfit;
     }
 }
