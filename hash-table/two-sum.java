bab9273497ebda2490c6a6920f46e47455ec469d
Omkar Shinde <70501103+omkarshinde1711@users.noreply.github.com>
2025-07-21 23:53:17 +0530

    LC-two-sum [Runtime: 2 ms; Memory: 44.9 MB]

--- hash-table/two-sum.java
+++ hash-table/two-sum.java
@@ -1,15 +1,13 @@
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int arr2[] = new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++)
-                if (nums[i]+nums[j] == target){
-                   arr2[0] = i;
-                   arr2[1] = j;
-            
-                }
+        HashMap <Integer, Integer> Checker = new HashMap <>();
+        for ( int i = 0; i < nums.length; i++){
+            int diff = target - nums[i]; 
+            if (Checker.containsKey(diff)){
+                return new int[]{Checker.get(diff),i};
+            }
+            Checker.put(nums[i],i);
         }
-        return arr2; 
-    }
+    throw new IllegalArgumentException("No two sum solution found");
+    }  
 }
